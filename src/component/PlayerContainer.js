5c7c26fcba414e15bf40eb13c47d6a52e1e7a81a
+ <+>
2022-05-11 09:54:33 -0400

    setup json server

--- src/component/PlayerContainer.js
+++ src/component/PlayerContainer.js
@@ -1,15 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
+import PlayerList from "./PlayerList";
 
 function PlayerContainer() {
+  const [players, setPlayers] = useState([]);
 
-  useEffect(() =>{
-    fetch('')
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:3001/players")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
-  return (
-    <div>PlayerContainer</div>
-  )
+  // const listOfPlayers = players.map((player) => {
+  //   <PlayerList player={player} key={player.id} />;
+  // });
+
+  return <div>PlayerContainer</div>;
 }
 
-export default PlayerContainer
-
+export default PlayerContainer;
--- src/component/PlayerList.js
+++ src/component/PlayerList.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-function PlayerList() {
+
+function PlayerList({player}) {
+  console.log(player)
+  const {id, ranking, name, image, country} = player
+
   return (
     <div>PlayerList</div>
   )
